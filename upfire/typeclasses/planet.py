d1e04a9328749435b0c103f180a36d50576f1591
Tymoteusz Paul <tymoteusz.paul@x20x.co.uk>
2018-05-20 02:46:38 +0100

    Adding basic planetary traits, resources and their handling on look

--- upfire/typeclasses/planet.py
+++ upfire/typeclasses/planet.py
@@ -1,5 +1,8 @@
+from evennia.utils import lazy_property
+
 from commands.planet import PlanetCommandSet
 from objects import Object
+from world.traits import TraitHandler
 
 
 class Planet(Object):
@@ -10,5 +13,51 @@ class Planet(Object):
     def at_object_creation(self):
         self.cmdset.add_default(PlanetCommandSet, permanent=True)
         self.locks.add("puppet:all();call:false()")
-        self.db.desc = "This is a planet still pending to be described."
-        self.db.focus = "civilian"
+        self.traits.add(
+            name="Population",
+            key="population",
+            type="counter",
+            base=0,
+        )
+        self.traits.add(
+            name="Temperature",
+            key="temperature",
+            type="static",
+            base=0,
+            mod=0,
+        )
+        self.resources.add(
+            name="Various metals",
+            key="metals",
+            type="counter",
+            base=0,
+        )
+        self.resources.add(
+            name="Various minerals",
+            key="minerals",
+            type="counter",
+            base=0,
+        )
+
+    def return_appearance(self, looker=None):
+        desc_string = "Planet %s \n" % self.name
+        desc_string += "\n----------------\n"
+        desc_string += "Traits:\n"
+        for trait in self.traits.all:
+            trait = self.traits[trait]
+            desc_string += str(trait.name) + ": " + str(trait.actual) + "\n"
+        desc_string += "\n----------------\n"
+        desc_string += "Resources:\n"
+        for resource in self.resources.all:
+            resource = self.resources[resource]
+            desc_string += str(resource.name) + ": " + str(
+                resource.actual) + "\n"
+        return desc_string
+
+    @lazy_property
+    def traits(self):
+        return TraitHandler(self)
+
+    @lazy_property
+    def resources(self):
+        return TraitHandler(self, db_attribute="resources")
