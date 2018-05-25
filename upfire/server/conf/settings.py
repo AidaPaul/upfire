c57e9289c714b922c08d6e35dfdae24bba784116
Tymoteusz Paul <tymoteusz.paul@x20x.co.uk>
2018-05-25 12:04:06 +0100

    Minor upkeeping

--- upfire/server/conf/settings.py
+++ upfire/server/conf/settings.py
@@ -1,42 +1,5 @@
-"""
-Evennia settings file.
-
-The available options are found in the default settings file found
-here:
-
-E:\Amazon Drive\PycharmProjects\upfire\venv\Lib\site-packages\evennia\settings_default.py
-
-Remember:
-
-Don't copy more from the default file than you actually intend to
-change; this will make sure that you don't overload upstream updates
-unnecessarily.
-
-When changing a setting requiring a file system path (like
-path/to/actual/file.py), use GAME_DIR and EVENNIA_DIR to reference
-your game folder and the Evennia library folders respectively. Python
-paths (path.to.module) should be given relative to the game's root
-folder (typeclasses.foo) whereas paths within the Evennia library
-needs to be given explicitly (evennia.foo).
-
-If you want to share your game dir, including its settings, you can
-put secret game- or server-specific settings in secret_settings.py.
-
-"""
-
-# Use the defaults from Evennia unless explicitly overridden
-from evennia.settings_default import *
-
-######################################################################
-# Evennia base server config
-######################################################################
-
 # This is the name of your game. Make it catchy!
 SERVERNAME = "upfire"
-
-# Server ports. If enabled and marked as "visible", the port
-# should be visible to the outside world on a production server.
-# Note that there are many more options available beyond these.
 
 # Telnet ports. Visible.
 TELNET_ENABLED = True
@@ -56,9 +19,6 @@ WEBSOCKET_CLIENT_PORT = 4005
 # Internal Server-Portal port. Not visible.
 AMP_PORT = 4006
 
-######################################################################
-# Settings given in secret_settings.py override those in this file.
-######################################################################
 try:
     from server.conf.secret_settings import *
 except ImportError:
